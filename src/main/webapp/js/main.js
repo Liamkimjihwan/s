d326ac271f6b5c9ec234f4c69ebfaf9b65f32dcf
Liamkimjihwan <wwwwwwlghskdlekd@naver.com>
2017-02-10 19:14:54 +0900

    slide 새로 추가

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,16 +3,16 @@ $( function() {
     var state = true;
     $( ".job-more" ).on( "click", function() {
       if ( state ) {
-  	    $(".rec-job2").hide().slideDown();
-	    $(".rec-job2").css("display", "inline-block");
+  	    $(".job2-conts").hide().slideDown();
+	    $(".job2-conts").css("display", "inline-block");
         $( "#job-effect" ).animate({
           backgroundColor: "#BDBDBD",
           color: "#fff",
           height: "600px"
         }, 1000 );
       } else {
-	  $(".rec-job2").show().slideUp();
-  	    $(".rec-job2").css("display", "none");
+	  $(".job2-conts").show().slideUp();
+  	    $(".job2-conts").css("display", "none");
         $( "#job-effect" ).animate({
           backgroundColor: "#fff",
           color: "#000",
@@ -27,16 +27,16 @@ $( function() {
     var state = true;
     $( ".mento-more" ).on( "click", function() {
       if ( state ) {
-  	    $(".rec-mento2").hide().slideDown();
-	    $(".rec-mento2").css("display", "inline-block");
+  	    $(".mento2-conts").hide().slideDown();
+	    $(".mento2-conts").css("display", "inline-block");
         $( "#mento-effect" ).animate({
           backgroundColor: "#BDBDBD",
           color: "#fff",
           height: "600px"
         }, 1000 );
       } else {
-	  $(".rec-mento2").show().slideUp();
-  	    $(".rec-mento2").css("display", "none");
+	  $(".mento2-conts").show().slideUp();
+  	    $(".mento2-conts").css("display", "none");
         $( "#mento-effect" ).animate({
           backgroundColor: "#fff",
           color: "#000",
@@ -51,16 +51,16 @@ $( function() {
     var state = true;
     $( ".video-more" ).on( "click", function() {
       if ( state ) {
-  	    $(".rec-video2").hide().slideDown();
-	    $(".rec-video2").css("display", "inline-block");
+  	    $(".video2-conts").hide().slideDown();
+	    $(".video2-conts").css("display", "inline-block");
         $( "#video-effect" ).animate({
           backgroundColor: "#BDBDBD",
           color: "#fff",
           height: "600px"
         }, 1000 );
       } else {
-	  $(".rec-video2").show().slideUp();
-  	    $(".rec-video2").css("display", "none");
+	  $(".video2-conts").show().slideUp();
+  	    $(".video2-conts").css("display", "none");
         $( "#video-effect" ).animate({
           backgroundColor: "#fff",
           color: "#000",
