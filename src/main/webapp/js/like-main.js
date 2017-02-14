4299a306d27c0df2de16c3f6197b5fb48f8ee4ba
Liamkimjihwan <wwwwwwlghskdlekd@naver.com>
2017-02-14 21:50:50 +0900

    추천 좋아요 페이지 만들기,,,

--- src/main/webapp/js/like-main.js
+++ src/main/webapp/js/like-main.js
@@ -24,32 +24,7 @@ $( function() {
       state = !state;
     });
     
-    var state = true;
-    $(document.body).on( "click", ".model-more", function() {
-      if ( state ) {
-    	  $(".job-all, .video-all, .all-rec-mento").show().slideUp();
-  	    $(".model2-conts").hide().slideDown();
-	    $(".model2-conts").css("display", "inline-block");
-	    
-        $( "#model-effect" ).animate({
-          backgroundColor: "#BDBDBD",
-          color: "#fff",
-          height: "600px"
-        }, 1000 );
-        $(".all-rec-mento, .job-all, .video-all").css("display", "none");
-      } else {
-    	  $(".all-rec-mento, .job-all, .video-all").hide().slideDown(1200);
-    	  $(".model2-conts").show().slideUp();
-  	    $(".model2-conts").css("display", "none");
-        $( "#model-effect" ).animate({
-          backgroundColor: "#fff",
-          color: "#000",
-          height: "250px"
-        }, 1000 );
-        $(".all-rec-mento, .job-all, .video-all").css("display", "inline-block");
-      }
-      state = !state;
-    });
+    
 
     var state = true;
     $(document.body).on( "click", ".mento-more", function() {
@@ -79,7 +54,7 @@ $( function() {
     });
 
     var state = true;
-    $(document.body).on( "click", ".video-more", function() {
+    $( ".video-more" ).on( "click", function() {
       if ( state ) {
   	    $(".video2-conts").hide().slideDown();
 	    $(".video2-conts").css("display", "inline-block");
@@ -102,51 +77,7 @@ $( function() {
       }
       state = !state;
     });
-   
-    var state = true;
-    $(document.body).on( "click", "#tab-2", function() {
-      if ( state ) {
-    	$("input:radio[class='like-list']").attr("checked", true);
-    	$("input:radio[class='rec-list']").attr("checked", false);
-	    $(".all-like-list").css("display", "inline-block");
-        $(".all-rec-list").css("display", "none");
-        $(".all-rec-model, .job-all, .all-rec-mento, video-all").css("display", "inline-block");
-      } else {
-    	  $("input:radio[class='rec-list']").attr("checked", true);
-      	$("input:radio[class='like-list']").attr("checked", false);
-  	    $(".all-like-list").css("display", "none");
-       
-        $(".all-rec-list").css("display", "inline-block");
-        $(".all-rec-model, .job-all, .all-rec-mento, video-all").css("display", "inline-block");
-      }
-      state = !state;
-      console.log(state);
-    });
-    
-    if("#tab-2".checked == true) {
-    	
-    	var state = true;
-    $(document.body).on( "click", "#tab-1", function() {
-      if ( state ) {
-    	$("input:radio[class='rec-list']").attr("checked", true);
-    	$("input:radio[class='like-list']").attr("checked", false);
-	    $(".all-rec-list").css("display", "inline-block");
-        $(".all-like-list").css("display", "none");
-        $(".all-rec-model, .job-all, .all-rec-mento, video-all").css("display", "inline-block");
-      } else {
-    	  $("input:radio[class='like-list']").attr("checked", true);
-      	$("input:radio[class='rec-list']").attr("checked", false);
-  	    $(".all-rec-list").css("display", "none");
-       
-        $(".all-like-list").css("display", "inline-block");
-        $(".all-rec-model, .job-all, .all-rec-mento, video-all").css("display", "inline-block");
-      }
-      state = !state;
-    });
-    }
   });
-
-
 
 	
 	
