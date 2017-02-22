c61d6692aa47b73cee2c0d5a990dddde4fb5cda9
Liamkimjihwan <wwwwwwlghskdlekd@naver.com>
2017-02-22 17:21:24 +0900

    크롤링 작업 main2.js

--- src/main/webapp/mystuff/js/main.js
+++ src/main/webapp/mystuff/js/main.js
@@ -1,5 +1,9 @@
  
 $( function() { 
+	
+	
+	
+	
     var state = true;
     $(document.body).on( "click", ".job-more", function() { // 직업 더보기
       if ( state ) {
@@ -125,7 +129,6 @@ $( function() {
    
     });
     
-
     
   /*  $(document.body).on( "click", ".favor-btn", function() {
     
@@ -142,7 +145,7 @@ $( function() {
   });*/
 });
 
-var state = true;
+/*var state = true;
 function changeimg() {// 좋아요 버튼 눌렀을 때
 	if(state) {
 		this.setAttribute( "src", "../image/fheart.png" );
@@ -158,7 +161,7 @@ function changeimg() {// 좋아요 버튼 눌렀을 때
 		$(".like-del").css("animation-delay", "1s");
 	}
 	state = !state;
-};
+};*/
 
 
 /*function ButtonDown() {  마우스로 눌렀을 때 이미지 변환  
@@ -175,9 +178,7 @@ function ButtonUp() {
 */
 
 	
-	
-	
-(function($) {
+(function($) { // 슬라이드 쇼
     $(function() {
         $('.jcarousel').jcarousel();
 
@@ -214,63 +215,40 @@ function ButtonUp() {
     });
 })(jQuery);
 
-	
 
-$(document).ready(function ($) {
+var request = require("request");  
+var cheerio = require("cheerio");  
+var url = "https://www.ted.com/talks?language=ko";
 
-    var jssor_1_SlideoTransitions = [
-/*      [{b:0,d:600,y:-290,e:{y:27}}],
-      [{b:0,d:1000,y:185},{b:1000,d:500,o:-1},{b:1500,d:500,o:1},{b:2000,d:1500,r:360},{b:3500,d:1000,rX:30},{b:4500,d:500,rX:-30},{b:5000,d:1000,rY:30},{b:6000,d:500,rY:-30},{b:6500,d:500,sX:1},{b:7000,d:500,sX:-1},{b:7500,d:500,sY:1},{b:8000,d:500,sY:-1},{b:8500,d:500,kX:30},{b:9000,d:500,kX:-30},{b:9500,d:500,kY:30},{b:10000,d:500,kY:-30},{b:10500,d:500,c:{x:87.50,t:-87.50}},{b:11000,d:500,c:{x:-87.50,t:87.50}}],
-      [{b:0,d:600,x:410,e:{x:27}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,o:1,e:{o:5}}],
-      [{b:-1,d:1,c:{x:175.0,t:-175.0}},{b:0,d:800,c:{x:-175.0,t:175.0},e:{c:{x:7,t:7}}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,x:-570,o:1,e:{x:6}}],
-      [{b:-1,d:1,o:-1,r:-180},{b:0,d:800,o:1,r:180,e:{r:7}}],
-      [{b:0,d:1000,y:80,e:{y:24}},{b:1000,d:1100,x:570,y:170,o:-1,r:30,sX:9,sY:9,e:{x:2,y:6,r:1,sX:5,sY:5}}],
-      [{b:2000,d:600,rY:30}],
-      [{b:0,d:500,x:-105},{b:500,d:500,x:230},{b:1000,d:500,y:-120},{b:1500,d:500,x:-70,y:120},{b:2600,d:500,y:-80},{b:3100,d:900,y:160,e:{y:24}}],
-      [{b:0,d:1000,o:-0.4,rX:2,rY:1},{b:1000,d:1000,rY:1},{b:2000,d:1000,rX:-1},{b:3000,d:1000,rY:-1},{b:4000,d:1000,o:0.4,rX:-1,rY:-1}]*/
-    ];
-
-    var jssor_1_options = {
-      $AutoPlay: true,
-      $Idle: 2000,
-      $CaptionSliderOptions: {
-        $Class: $JssorCaptionSlideo$,
-        $Transitions: jssor_1_SlideoTransitions,
-        $Breaks: [
-          [{d:2000,b:1000}]
-        ]
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
-
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-    /*responsive code begin*/
-    /*you can remove responsive code if you don't want the slider scales while window resizing*/
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 600);
-            jssor_1_slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
+request(url, function (err, res, html) {
+    if (!err) {
+        var $ = cheerio.load(html);
+        
+        // 블로그 title 정보 가져오기
+        $(".entry-title > a").each(function () {
+        	console.log(this);
+            var post = {"title": "", "link": "", "summary": "", "category": []};
+            var data = $(this);
+            
+            post["title"] = data.text();
+            post["link"] = data.attr("href");
+        });
+        
+        // 블로그 요약 정보 가져오기
+        $(".entry-summary > p").each(function (i) {
+            // do something
+        })
+ 
+        // 블로그 카테고리 가져오기
+        $(".entry-categories").each(function (i) {
+            $(this).children('a').each(function () {
+                // do something
+            });
+        })
     }
-    ScaleSlider();
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    /*responsive code end*/
-});
-	
-	
+})
+
+
+
 	
 	
---
+++ src/main/webapp/mystuff/js/main2.js
@@ -0,0 +1,83 @@
+var request = require("request");  
+var cheerio = require("cheerio");  
+var url = "https://www.ted.com/talks?language=ko";
+
+request(url, function(error, response, html){  
+    if (error) {throw error};
+
+    //console.log (html);
+
+    var $ = cheerio.load(html);
+    
+/*    $("div.media__image").each(function(){
+//    	console.log("djkdjkw");
+        $(this).children(".thumb__tugger").each(function(){
+        	console.log("djkdjkws");
+            var post = {"video":"","image":""};        
+        	var mom = $("div.media__image");
+        	var thumb = mom.children("img.thumb__image");
+        	
+        	post["video"] = mom.children("a").attr("href");
+        	post["image"] = thumb.attr("src");
+        	console.log(post["video"]);
+        })
+    
+    });*/
+    
+
+//var post = "";
+ $("div.media__image").each(function(){
+  var value = {"video":""};
+  value["video"] = $(this).children("a").attr("href"); 
+    	console.log(value);
+    });
+    
+    $("img.thumb__image").each(function(){
+    	  var value = {"image":""};
+    	  value["image"] = $(this).attr("src"); 
+    	console.log(value);
+    });
+
+   $('div.media__message h4.h12').each(function(){
+        console.log("작성자 : " + $(this).text());
+    })
+    
+    $('div.media__message h4.h9').each(function(){
+        console.log("제목 : " + $(this).text());
+    })
+    
+});
+
+
+/*request(url, function (err, res, html) {
+    if (!err) {
+        var $ = cheerio.load(html);
+        
+        // 블로그 title 정보 가져오기
+        $(".entry-title > a").each(function () {
+        	console.log(this);
+            var post = {"title": "", "link": "", "summary": "", "category": []};
+            var data = $(this);
+            
+            post["title"] = data.text();
+            post["link"] = data.attr("href");
+        });
+        
+        // 블로그 요약 정보 가져오기
+        $(".entry-summary > p").each(function (i) {
+            // do something
+        })
+ 
+        // 블로그 카테고리 가져오기
+        $(".entry-categories").each(function (i) {
+            $(this).children('a').each(function () {
+                // do something
+            });
+        })
+    }
+})
+*/
+
+
+	
+	
