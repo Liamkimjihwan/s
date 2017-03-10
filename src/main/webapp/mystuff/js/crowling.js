24352df7d6bda2caa17b254763c089e91dd2ed91
Liamkimjihwan <wwwwwwlghskdlekd@naver.com>
2017-03-10 19:31:15 +0900

    ..

--- src/main/webapp/mystuff/js/crowling.js
+++ src/main/webapp/mystuff/js/crowling.js
@@ -1,84 +1,143 @@
+var mysql = require('mysql');
 var request = require("request");  
 var cheerio = require("cheerio");  
-var url = "https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=architecture";
-
-
-
-request(url, function(error, response, html){  
-    if (error) {throw error};
-
-    var a = cheerio.load(html);
-    
-/*    $("div.media__image").each(function(){
-//    	console.log("djkdjkw");
-        $(this).children(".thumb__tugger").each(function(){
-        	console.log("djkdjkws");
-            var post = {"video":"","image":""};        
-        	var mom = $("div.media__image");
-        	var thumb = mom.children("img.thumb__image");
-        	
-        	post["video"] = mom.children("a").attr("href");
-        	post["image"] = thumb.attr("src");
-        	console.log(post["video"]);
-        })
-    });*/
-    
-//var value = "";
- a("div.media__image").each(function(){
-   value = {"video":""};
-  value["video"] = a(this).children("a").attr("href"); 
-  console.log(value);
-    });
-    
-    a("img.thumb__image").each(function(){
-    	  value = {"image":""};
-    	  value["image"] = a(this).attr("src"); 
-    	console.log(value);
-    });
-
-   a('div.media__message h4.h12').each(function(){
-        console.log("작성자 : " + a(this).text());
-    })
-    
-    a('div.media__message h4.h9').each(function(){
-        console.log("제목 : " + a(this).text());
-    })
-    
-});
-console.log(request);
-
-
-/*request(url, function (err, res, html) {
-    if (!err) {
-        var $ = cheerio.load(html);
-        
-        // 블로그 title 정보 가져오기
-        $(".entry-title > a").each(function () {
-        	console.log(this);
-            var post = {"title": "", "link": "", "summary": "", "category": []};
-            var data = $(this);
-            
-            post["title"] = data.text();
-            post["link"] = data.attr("href");
-        });
-        
-        // 블로그 요약 정보 가져오기
-        $(".entry-summary > p").each(function (i) {
-            // do something
-        })
- 
-        // 블로그 카테고리 가져오기
-        $(".entry-categories").each(function (i) {
-            $(this).children('a').each(function () {
-                // do something
-            });
-        })
-    }
-})
+var url = "https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=astronomy";
+/*https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=astronomy
+   https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=biotech
+      https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=finance
 */
+var dbConnection = mysql.createConnection({   
+    host: 'localhost', 
+    user: 'java89',   
+    password: '1111',   
+    database: 'dreamtree' 
+   });
 
 
 
 
-	
-	
+
+
+
+request(url, function(error, response, html){
+       if (error) {throw error};
+   
+       var a = cheerio.load(html);
+       
+       var cono = 0;
+       var count = 0;
+       var crtitle = new Array(); 
+       var anker = new Array();
+       var thumImg = new Array();
+       var author = new Array();
+       var posted = new Array();
+       var vodsc = new Array(); 
+       var simg = new Array(); 
+       var spnm = new Array(); 
+       var spdsc = new Array();
+       var posted = new Array();
+
+       a('div.media__message h4.h9').each(function(){ // 제목
+          crtitle[count++] = a(this).text().replace(/\n/g, "").replace(/\r/g, "");
+       });
+       count= 0;
+       a('div.media__image').each(function(){ // 영상 넘어가는 주소
+          anker[count++] = "https://www.ted.com" + a(this).children("a").attr("href");
+//          console.log(anker[count-1]);
+       });
+       count= 0;
+       a('img.thumb__image').each(function(){ // 비디오 썸네일
+          thumImg[count++] = a(this).attr("src");
+       });
+       count= 0;
+       a('div.media__message h4.h12').each(function(){ // speaker 이름
+          author[count++] = a(this).text();
+       });
+       count= 0;
+       a('div.media__message .meta__item .meta__val').each(function(){ // 날짜
+          post = a(this).text().replace(/\n/g, "").split(" ");
+          switch(post[0]) {
+          case "Jan": post[0] = "0"+1; break;
+          case "Feb": post[0] = "0"+2; break;
+          case "Mar": post[0] = "0"+3; break;
+          case "Apr": post[0] = "0"+4; break;
+          case "May": post[0] = "0"+5; break;
+          case "Jun": post[0] = "0"+6; break;
+          case "Jul": post[0] = "0"+7; break;
+          case "Aug": post[0] = "0"+8; break;
+          case "Sep": post[0] = "0"+9; break;
+          case "Oct": post[0] = 10; break;
+          case "Nov": post[0] = 11; break;
+          case "Dec": post[0] = 12; break;
+          }
+          posted[count++] = post[1] + post[0];
+//            date[count++]
+        });
+//       console.log(posted);
+//       console.log(date.length);
+        count= 0;
+        
+        
+
+       for (i = 0; anker.length > i; i++) {
+    	   url = anker[i]; 
+    	               crowl(url); 
+    	               
+    	             	dbConnection.query(
+    	                		  'select cono from contents ORDER BY cono desc', 
+    	                		  function(err, rows, fields) { // 서버에서 결과를 받았을 때 호출되는 함수
+    	                			  // connection.query () 을 실행하고 결과가 나온다음 이 function을 실행해라.
+  
+    	                				var cono = rows[0].cono +1;
+    	                		
+    	                				console.log(cono);
+
+    	             dbConnection.query("insert into contents(cono,type) values(?,?)", 
+    	      					   [cono, "video"],
+    	      					   function (err, rows, fields) {
+    	  /* 				if(cono == rows[0].cono) {
+    	    				  return cono +1;
+    	    			  }*/
+    	          	   if (err) throw err;
+//    	      				   			console.log(rows);
+    	             });			
+    	             	
+    	  			   dbConnection.query("insert into video(cono, kotl, entl, voimg, vodsc, spnm, sjob, simg,posted) values(?,?,?,?,?,?,?,?,?)", 
+    	  					   [cono, crtitle[count], anker[count], thumImg[count], vodsc[count], spnm[count], spdsc[count], simg[count], posted[count]],
+    	  					   function (err, rows, fields) {
+    	  /*     				if(cono == rows[0].cono) {
+    	        				   cono +1;
+    	        			  }*/
+    	       				if (err) throw err;
+//    	  				   			console.log(rows);
+    	  			   			});
+    	  			   
+
+    	  			   
+
+    	       		});
+
+       }
+       
+       var a;
+//       var cono = 8; 
+       function crowl(url) {
+    	   request(url, function(error, response, html){  
+    		   
+    		   count++
+    		   a = cheerio.load(html);
+    		   
+    		   vodsc[count] = a('p.talk-description').text().replace(/\n/g, "").replace(/\r/g, "");
+    		   simg[count] = a('img.thumb__image').attr('src').replace("?", "");
+    		   spnm[count] = a('a.talk-speaker__link').attr("href");
+    		   spdsc[count] = a('div.talk-speaker__description').text(); // 스피커 직업
+//    		   console.log(count + "in " + "spdsc " + spdsc[count] + "  vodsc " + vodsc[count]);
+    		   
+
+            
+//             	dbConnection.end();
+			   
+			   
+	   });
+   }
+});
--- src/main/webapp/mystuff/js/crowling_1.js
+++ src/main/webapp/mystuff/js/crowling_1.js
@@ -3,87 +3,74 @@ var request = require("request");
 var cheerio = require("cheerio");  
 var url = "https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=astronomy";
 /*https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=astronomy
-	https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=biotech
-		https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=finance
-*/var dbConnection = mysql.createConnection({   
+   https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=biotech
+      https://www.ted.com/talks?language=ko&sort=newest&topics%5B%5D=finance
+*/
+var dbConnection = mysql.createConnection({   
     host: 'localhost', 
     user: 'java89',   
     password: '1111',   
     database: 'dreamtree' 
    });
 
-request(url, function(error, response, html){  
-	    if (error) {throw error};
-	
-	    var a = cheerio.load(html);
+request(url, function(error, response, html){
+       if (error) {throw error};
+   
+       var a = cheerio.load(html);
 
-	    var count = 0;
-	    var crtitle = new Array(); 
-	    var anker = new Array();
-	    var thumImg = new Array();
-	    var author = new Array();    
-	    var vodsc = new Array(); 
-	    var simg = new Array(); 
-	    var spnm = new Array(); 
-	    var spdsc = new Array();
+       var count = 0;
+       var crtitle = new Array(); 
+       var anker = new Array();
+       var thumImg = new Array();
+       var author = new Array();    
+       var vodsc = new Array(); 
+       var simg = new Array(); 
+       var spnm = new Array(); 
+       var spdsc = new Array();
 
-	    a('div.media__message h4.h9').each(function(){ // 제목
-	    	crtitle[count++] = a(this).text().replace(/\n/g, "").replace(/\r/g, "");
-	    });
-	    count= 0;
-	    a('div.media__image').each(function(){ // 제목
-	    	anker[count++] = "https://www.ted.com" + a(this).children("a").attr("href");
-	    });
-	    for (var i= 0; i < anker.length; i++) {
-	    	console.log("==============================");
-	    	console.log("anker확인" + anker[i]);
-	    	console.log("번호" + i);
-	    	
-	    }
-	    count= 0;
-	    a('img.thumb__image').each(function(){ // 제목
-	    	thumImg[count++] = a(this).attr("src");
-	    });
-	    count= 0;
-	    a('div.media__message h4.h12').each(function(){ // 제목
-	    	author[count++] = a(this).text();
-	    });
-	    count = 0;
-	    
-	    
-/*	    var url2 = Array();
-	    for (var i= 0; i < anker.length; i++) {
-	    	url2 = anker[i];
-	    	console.log("anker확인" + url2);
-	    }*/
-	    
-	    var url2 = Array();
-	    for (var i= 0; i < anker.length; i++) {
-	    	url2 = anker[i];
-	    request(url2, function(error, response, html){
-	    	console.log("anker확인" + url2);
-		    if (error) {throw error};
-		
-		    a = cheerio.load(html);
-			
-		    vodsc[i] = a('p.talk-description').text().replace(/\n/g, "").replace(/\r/g, "");
-		    simg[i] = a('img.thumb__image').attr("src");
-		    spnm[i] = a('a.talk-speaker__link').attr("href");
-		    spdsc[i++] = a('div.talk-speaker__description').text();
-//		    console.log("in " + "spdsc " + spdsc.length + "  vodsc " + vodsc.length);
-		});
-	    
-	    }
-//	    console.log("out " + "spdsc " + spdsc.length + "  vodsc " + vodsc.length);
-    
-	    
-//	    for(var i = 0; i < crtitle.length; i++) {
-//	    	cono = 8; 
-//	    	/*console.log(i + "타이틀" + "[" + crtitle[i]+"]" +"밸류"+ "[" +value[i] + "]" + "썸네일" + "[" +thumImg[i] + "]"+ "작가"+"[" +author[i]+ "]");*/
-//	    	dbConnection.query("insert into video(cono, kotl, entl, voimg, vodsc, spnm, sjob, simg) values(?,?,?,?,?,?,?,?)", 
-//	    			[cono++, crtitle[i], anker[i], thumImg[i], vodsc[i], spnm[i], spdsc[i], simg[i]], 
-//	    			function (err, rows, fields) {
-////	    		console.log(rows);
-//	    	});
-//	    }
+       a('div.media__message h4.h9').each(function(){ // 제목
+          crtitle[count++] = a(this).text().replace(/\n/g, "").replace(/\r/g, "");
+       });
+       count= 0;
+       a('div.media__image').each(function(){ // 영상 넘어가는 주소
+          anker[count++] = "https://www.ted.com" + a(this).children("a").attr("href");
+//          console.log(anker[count-1]);
+       });
+       count= 0;
+       a('img.thumb__image').each(function(){ // 비디오 썸네일
+          thumImg[count++] = a(this).attr("src");
+       });
+       count= 0;
+       a('div.media__message h4.h12').each(function(){ // speaker 이름
+          author[count++] = a(this).text();
+       });
+       
+       count= 0;
+       for (i = 0; anker.length > i; i++) {
+    	   url = anker[i]; 
+    	   crowl(); 
+       }
+       
+       var a;
+       var cono = 8; 
+       function crowl() {
+    	   request(url, function(error, response, html){  
+    		   if (error) {throw error};
+    		   
+    		   count++
+    		   a = cheerio.load(html);
+    		   
+    		   vodsc[count] = a('p.talk-description').text().replace(/\n/g, "").replace(/\r/g, "");
+    		   simg[count] = a('img.thumb__image').attr("src").replace("?", "");
+    		   spnm[count] = a('a.talk-speaker__link').attr("href");
+    		   spdsc[count] = a('div.talk-speaker__description').text(); // 스피커 직업
+//    		   console.log(count + "in " + "spdsc " + spdsc[count] + "  vodsc " + vodsc[count]);
+    		   
+			   dbConnection.query("insert into video(cono, kotl, entl, voimg, vodsc, spnm, sjob, simg) values(?,?,?,?,?,?,?,?)", 
+					   [cono++, crtitle[count], anker[count], thumImg[count], vodsc[count], spnm[count], spdsc[count], simg[count]], 
+					   function (err, rows, fields) {
+				   			console.log(rows);
+			   			});
+	   });
+   }
 });
